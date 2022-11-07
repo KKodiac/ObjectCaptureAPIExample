8c8d64d26d32f0a94517c1ea04d756b6361a78d2
홍성민 <seanhong2000@gmail.com>
2022-11-07 13:15:27 +0900

    Initial Commit

---
+++ HeadScanner/HeadScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadScannerApp.swift
+//  HeadScanner
+//
+//  Created by Sean Hong on 2022/11/07.
+//
+
+import SwiftUI
+
+@main
+struct HeadScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
